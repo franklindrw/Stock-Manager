cccb343fcf609c05ef6594ee89393cf36df53835
Franklin Campos <franklindrw@gmail.com>
2026-02-14 17:44:11 +0000

    config: adicionando formatacao pelo ruff

--- src/infra/models/inventory_balance_model.py
+++ src/infra/models/inventory_balance_model.py
@@ -1,21 +1,24 @@
-from sqlalchemy import ForeignKey, CheckConstraint, text
-from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
+
+from sqlalchemy import CheckConstraint, ForeignKey, text
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base_model import Base
 from .products_model import ProductsModel
 from .stores_model import StoresModel
 
+
 class InventoryBalanceModel(Base):
     __tablename__ = "inventory_balances"
 
-    product_id: Mapped[UUID] = mapped_column(ForeignKey("products.id"), primary_key=True)
+    product_id: Mapped[UUID] = mapped_column(
+        ForeignKey("products.id"), primary_key=True
+    )
     store_id: Mapped[UUID] = mapped_column(ForeignKey("stores.id"), primary_key=True)
     available_quantity: Mapped[int] = mapped_column(default=0)
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=text("CURRENT_TIMESTAMP"),
-        onupdate=text("CURRENT_TIMESTAMP")
+        server_default=text("CURRENT_TIMESTAMP"), onupdate=text("CURRENT_TIMESTAMP")
     )
 
     product: Mapped["ProductsModel"] = relationship()
