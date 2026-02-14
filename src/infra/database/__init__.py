169bfdedeb49517c6e63c770d1824f7e33dddf3c
Franklin Campos <franklindrw@gmail.com>
2026-02-14 17:12:47 +0000

    refact: excluindo arquivos nao usados

--- src/infra/database/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import Base, ProductsModel, StoresModel, InventoryMovementModel, InventoryBalanceModel
-
-__all__ = ["Base", "ProductsModel", "StoresModel", "InventoryMovementModel", "InventoryBalanceModel"]
